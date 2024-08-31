ab6849cbeb2a87c89b4290a6e61d5557f389fa42
Douxx <136928935+douxxu@users.noreply.github.com>
2024-08-31 16:12:10 +0200

    The setup file located at piwave.hs.vc

---
+++ setup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Function to print text in magenta color
+print_magenta() {
+  echo -e "\e[35m$1\e[0m"
+}
+
+# Function to check the status of the last command and exit if it failed
+check_status() {
+  if [ $? -ne 0 ]; then
+    echo -e "\e[31mError: $1 failed\e[0m"  # Print error message in red
+    exit 1
+  fi
+}
+
+# Display ASCII art in magenta
+print_magenta "
+  ____  ___        __                   |                       !
+ |  _ \(_) \      / /_ ___      _____    |                       |
+ | |_) | |\ \ /\ / / _\` \ \ /\ / / _ \   |    |~/                |
+ |  __/| | \ V  V / (_| |\ V  V /  __/   |   _|~                |
+ |_|   |_|  \_/\_/ \__,_| \_/\_/ \___|   |  (_|   |~/            |
+                                       |      _|~                |
+                       .============.|  (_|   |~/              |
+                     .-;____________;|.      _|~                |
+                     | [_________I__] |     (_|                |
+                     |  \"\"\"\"\"  (_) (_) |                            |
+                     | .=====..=====. |                            |
+                     | |:::::||:::::| |                            |
+                     | '=====''=====' |                            |
+                     '----------------'                            |
+"
+
+# Execute installation commands
+echo "Creating the PiWave_deps directory..."
+mkdir -p PiWave_deps
+check_status "Creating directory PiWave_deps"
+
+echo "Changing to the PiWave_deps directory..."
+cd PiWave_deps || { echo -e "\e[31mError: cd to PiWave_deps failed\e[0m"; exit 1; }
+
+echo "Updating package lists..."
+sudo apt update
+check_status "Updating package lists"
+
+echo "Installing required packages..."
+sudo apt install -y python3 python3-pip libsndfile1-dev make ffmpeg
+check_status "Installing required packages"
+
+echo "Installing PiWave..."
+sudo pip install git+https://github.com/douxxu/piwave.git
+check_status "Installing PiWave"
+
+echo "Cloning PiFmRds..."
+sudo git clone https://github.com/ChristopheJacquet/PiFmRds
+check_status "Cloning PiFmRds"
+
+echo "Changing to PiFmRds/src directory..."
+cd PiFmRds/src || { echo -e "\e[31mError: cd to PiFmRds/src failed\e[0m"; exit 1; }
+
+echo "Cleaning previous builds..."
+make clean
+check_status "Cleaning previous builds"
+
+echo "Building PiFmRds..."
+make
+check_status "Building PiFmRds"
+
+echo "Returning to the previous directory..."
+cd ../../../ || { echo -e "\e[31mError: cd back to previous directory failed\e[0m"; exit 1; }
+
+echo "Setup completed successfully!"
